d88d6d6d51c58bc2245e2875e18e3854deafd43f
nienow <system@randombits.dev>
2023-08-27 21:07:49 -0500

    Add only start log

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 import './index.scss';
 import {createRoot} from "react-dom/client";
-import snApi from "./api/snApi";
 import LogOutput from "./components/LogOutput";
 
 const root = createRoot(document.getElementById('root'));
-const logs = [];
+const logs: (string | Error)[] = ['start'];
 let debounce;
 
 export const rerenderRoot = () => {
@@ -15,19 +14,21 @@ export const rerenderRoot = () => {
   );
 };
 
-snApi.initialize({
-  debounceSave: 400,
-  logObserver: (msgOrError) => {
-    logs.push(msgOrError);
-    if (debounce) {
-      clearTimeout(debounce);
-    }
-    debounce = setTimeout(() => {
-      rerenderRoot();
-    }, 1000);
-  }
-});
+rerenderRoot();
 
-snApi.subscribe(() => {
-  rerenderRoot();
-});
+// snApi.initialize({
+//   debounceSave: 400,
+//   logObserver: (msgOrError) => {
+//     logs.push(msgOrError);
+//     if (debounce) {
+//       clearTimeout(debounce);
+//     }
+//     debounce = setTimeout(() => {
+//       rerenderRoot();
+//     }, 1000);
+//   }
+// });
+//
+// snApi.subscribe(() => {
+//   rerenderRoot();
+// });
